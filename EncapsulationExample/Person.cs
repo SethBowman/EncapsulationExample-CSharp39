9cf0d329bf9b7d4fcc9a531ac48ceab2a16665cb
Seth Bowman <brandonsethbowman@gmail.com>
2023-08-01 11:52:39 -0500

    Add project files.

---
+++ EncapsulationExample/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    public class Person
+    {
+        //private field
+        private double _wallet = 0;
+
+        //properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //methods that can access our field
+        public void AddMoney(double moneyToAdd)
+        {
+            _wallet += moneyToAdd;
+        }
+
+        public double CheckWallet()
+        {
+            return _wallet;
+        }
+    }
+}
---
+++ EncapsulationExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using EncapsulationExample;
+
+
+var seth = new Person();
+
+var myWallet = seth.CheckWallet();
+
+Console.WriteLine("Money in wallet:");
+Console.WriteLine(myWallet);
+
+Console.WriteLine("Adding money..");
+seth.AddMoney(1000.00);
+
+var newWallet = seth.CheckWallet();
+Console.WriteLine(newWallet);
+
